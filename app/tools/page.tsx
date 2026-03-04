32d88f6645a583c427c5f060ecef6ee7c561e9c3
Saroj Adhikari <sarojadh1111@gmail.com>
2026-03-04 21:30:46 +0545

    Tools

--- app/tools/page.tsx
+++ app/tools/page.tsx
@@ -1,4 +1,35 @@
+import Container from "../components/container";
+import Items from "../components/items";
+import { IconName } from "lucide-react/dynamic";
+
 const Tools = () => {
-  return <>Tools</>;
+  interface Tool {
+    name: string;
+    description: string;
+    icon: IconName;
+    route: string;
+  }
+
+  const tools: Tool[] = [
+    {
+      name: "Installation Tools",
+      description: "Manage installation tool records",
+      icon: "wrench",
+      route: "installationtools",
+    },
+    {
+      name: "Calibration Tools",
+      description: "Track calibration tools and usage",
+      icon: "ruler",
+      route: "calibrationtools",
+    },
+  ];
+
+  return (
+    <Container>
+      <Items clients={tools} cols={2} />
+    </Container>
+  );
 };
+
 export default Tools;
